a6f7c263d2114c60d51faa8c98eaf3d45d5e20c9
alstntoot <alstntoot@gmail.com>
2022-04-23 22:39:31 +0900

    Step2

---
+++ Shared/Memory/Memo.swift
@@ -0,0 +1,22 @@
+//
+//  Memo.swift
+//  SwiftUiMemo
+//
+//  Created by Minsoo Jang on 2022/04/23.
+//
+
+import Foundation
+import SwiftUI
+
+class Memo: Identifiable, ObservableObject {
+    let id: UUID
+    @Published var content: String
+    let insertDate: Date
+    
+    init(content: String, insertDate: Date = Date.now){
+        id = UUID()
+        self.content = content
+        self.insertDate = insertDate
+    }
+    
+}
